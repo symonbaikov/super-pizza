527e5853f426ab9fc0a3ead73a8fc164113bd513
MostOfLuck <peterbaikov12@gmail.com>
2025-04-03 12:49:19 +0300

    Добавлена функция createOrder для обработки заказов, включая создание заказа, очистку корзины и отправку уведомлений по электронной почте.

--- app/actions.ts
+++ app/actions.ts
@@ -10,6 +10,86 @@ import { Prisma } from "@prisma/client";
 import { hashSync } from "bcrypt";
 import { cookies } from "next/headers";
 
+export async function createOrder(data: CheckoutFormValues) {
+  try {
+    const cookieStore = cookies();
+    const cartToken = cookieStore.get("cartToken")?.value;
+
+    if (!cartToken) {
+      throw new Error("Cart token not found");
+    }
+
+    /* Находим корзину по токену */
+    const userCart = await prisma.cart.findFirst({
+      include: {
+        user: true,
+        items: {
+          include: {
+            ingredients: true,
+            productItem: {
+              include: {
+                product: true,
+              },
+            },
+          },
+        },
+      },
+      where: {
+        token: cartToken,
+      },
+    });
+
+    /* Если корзина не найдена возращаем ошибку */
+    if (!userCart) {
+      throw new Error("Cart not found");
+    }
+
+    /* Если корзина пустая возращаем ошибку */
+    if (userCart?.totalAmount === 0) {
+      throw new Error("Cart is empty");
+    }
+
+    /* Создаем заказ */
+    const order = await prisma.order.create({
+      data: {
+        token: cartToken,
+        fullName: data.firstName + " " + data.lastName,
+        email: data.email,
+        phone: data.phone,
+        address: data.address,
+        comment: data.comment,
+        totalAmount: userCart.totalAmount,
+        items: JSON.stringify(userCart.items),
+      },
+    });
+
+    /* Очищаем корзину */
+    await prisma.cart.update({
+      where: {
+        id: userCart.id,
+      },
+      data: {
+        totalAmount: 0,
+      },
+    });
+
+    await prisma.cartItem.deleteMany({
+      where: {
+        cartId: userCart.id,
+      },
+    });
+
+    await sendEmail(
+      data.email,
+      "Next Pizza / Zaplaťte za svou objednávku #" + order.id,
+      PayOrderTemplate({
+        totalAmount: order.totalAmount,
+      })
+    );
+  } catch (err) {
+    console.log("[CreateOrder] Server error", err);
+  }
+}
 
 export async function updateUserInfo(body: Prisma.UserUpdateInput) {
   try {
